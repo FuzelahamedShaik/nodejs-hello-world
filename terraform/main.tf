c334e04c83587dbafc11321e33a0688633e7acdb
FuzelahamedShaik <fuzelahamed1999@gmail.com>
2024-06-06 20:07:57 +0300

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello-world"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world"
+      image     = "YOUR_DOCKERHUB_USERNAME/hello-world:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
